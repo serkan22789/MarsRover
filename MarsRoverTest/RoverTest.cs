dbeeb2ad730e4b7677be9e1401589acdc6e7ad82
Serkan Yıldırım <serkan.yildirim@ericssonmsp.com>
2020-03-05 09:16:30 +0300

    test project added

---
+++ MarsRoverTest/RoverTest.cs
@@ -0,0 +1,37 @@
+using MarsRoverNetCore;
+using MarsRoverNetCore.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverTest
+{
+    [TestClass]
+    public class RoverTest
+    {
+        [TestMethod]
+        public void FirstRoverCheckOutput()
+        {
+            Plateau Plateau = new Plateau(5, 5);
+            Rover rover = new Rover(new Point(1, 2, "N"));
+            Plateau.AddRover(rover, "LMLMLMLMM");
+            Assert.AreEqual(rover.ToString(), "1 3 N");
+        }
+
+        [TestMethod]
+        public void SecondRoverCheckOutput()
+        {
+            Plateau Plateau = new Plateau(5, 5);
+            Rover rover = new Rover(new Point(3, 3, "E"));
+            Plateau.AddRover(rover, "MMRMMRMRRM");
+            Assert.AreEqual(rover.ToString(), "5 1 E");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void IncorrectCommand()
+        {
+            Plateau Plateau = new Plateau(5, 5);
+            Rover rover = new Rover(new Point(3, 3, "E"));
+            Plateau.AddRover(rover, "AMMRMMRMRRM");
+        }
+    }
+}
